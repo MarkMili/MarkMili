cb48e231df1ffd7936a99a0d54fcb1dbf78753b4
MarkMili <markmilinovic24@gmail.com>
2019-11-07 10:25:21 +0100

    added 2 int values and write them out

--- Program.java
+++ Program.java
@@ -1,7 +1,10 @@
 public class Program
 {
-  public static void main(String[] args)throws IOException
+  public static void main(String[] args)
   {
     System.out.println("Vnesite ime in priimek");
+    int a=2;
+    int a=7;
+    System.out.println(a+""+b);
   }
 }
