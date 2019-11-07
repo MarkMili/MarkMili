840588c2df0c9bfc42913e9fa9eed2472be08ca1
MarkMili <markmilinovic24@gmail.com>
2019-11-07 12:36:43 +0100

    changes were made

--- Program.java
+++ Program.java
@@ -1,10 +1,25 @@
+import java.io.*;
 public class Program
 {
-  public static void main(String[] args)
+  public static void main(String[] args)throws IOException
   {
-    System.out.println("Vnesite ime in priimek");
-    int a=2;
-    int a=7;
-    System.out.println(a+""+b);
+    BufferedReader prva=new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+    System.out.println("Vnesite poljubno besedo:");
+    String a=prva.readLine();
+    System.out.println(a);
+    int b=2;
+    int c=7;
+    System.out.println(b+" "+c);
+    System.out.println("Preverimo ce se lahko beseda pretvori v niz:");
+    //static int parseInt(String s)
+    try{
+  int stevilka = Integer.parseInt(a);
+  System.out.println(stevilka+"    Niz se lahko pretvori v stevilo.");
+} 
+    catch (NumberFormatException e) 
+    {
+  System.out.println("Niz se ne more pretvorit v stevilo.");
+    }
   }
 }
+  
